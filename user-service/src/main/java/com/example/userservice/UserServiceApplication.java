f9af8ae4a36445c83987c9fe7b9ae715f242bb97
shravanhariharan2 <smhariha@ucsd.edu>
2020-06-07 17:22:37 -0700

    Setup Eureka clients

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,9 +2,11 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public WebClient.Builder getBuilder() {
